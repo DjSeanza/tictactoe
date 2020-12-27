8f3f529d0876ab0afded6aa0a60a3788260bcf9b
DjSeanza <33281052+DjSeanza@users.noreply.github.com>
2020-12-27 16:15:16 +0100

    Add files via upload
    dokončené pridávanie hráčov tak, aby nemohli mať rovnaký znak a taktiež pridané odstraňovanie hráčov

--- TicTacToe/Hra.java
+++ TicTacToe/Hra.java
@@ -19,8 +19,8 @@ public class Hra {
      * @param velkost je to veľkosť hracej plochy, plocha je vždy štvorcová
      * @param pocetHracov počet hráčov
      * 
-     * @TODO - aby hráč nemohol pridať rovnaký znak ako ma iný hráč
-     * @TODO - ošetriť počet hráčov tak, aby ich na hracej ploche nebolo veľa
+     * @TODO - počet výherných zmeniť na pocetZnakovZaSebou a počet výherných
+     * dať na ľubovoľný počet opakovaní tej hry.
      */
     public Hra(int velkost, int pocetVyhernych, int pocetHracov) { 
         this.hraci = new ArrayList<Hrac>();
@@ -30,12 +30,77 @@ public class Hra {
         this.hraciaPlocha.vypisPlochu();        
         
         for (int i = 0; i < pocetHracov; i++) {
-            System.out.println("Znak hráča: " + (i + 1));
-            char znak = this.input.next().charAt(0);
-            this.hraci.add(new Hrac(znak));
+            boolean jePridany = this.pridajHraca();
+            while (!jePridany) {
+                jePridany = this.pridajHraca();
+            }
         }
         
         this.pocetHracov = pocetHracov;
+    }
+    
+    /**
+     * Metóda pridáva hráčov do hry.
+     * 
+     * @return boolean vráti hodnotu true ak sa hráč úspešne pridal
+     */
+    public boolean pridajHraca() {
+        /* this.hraciaPlocha.getVelkostPlochy() - 2
+         * pretože do veľkosti plochy sa rátajú aj stĺpce a riadky s číslami
+         * a hráčov má byť aspoň o jedného menej ako je veľkosť plochy.
+        */
+        if (this.hraci.size() >= this.hraciaPlocha.getVelkostPlochy() - 2) {
+            System.out.println("Príliš veľa hráčov, nedá sa pridať viac.");
+            return false;
+        }
+        
+        System.out.println("Znak hráča: " + (this.hraci.size() + 1));
+        char znak = this.input.next().charAt(0);
+        boolean jeZnakRovnaky = false;
+        
+        for (int i = 0; i < this.hraci.size(); i++) {
+            if (znak == this.hraci.get(i).getZnak()) {
+                System.out.println("Takýto znak má už iný hráč. Zadajte nový.");
+                jeZnakRovnaky = true;
+            }
+        }
+        
+        if (!jeZnakRovnaky) {
+            this.pocetHracov++;
+            return this.hraci.add(new Hrac(znak));
+        }
+        
+        return false;
+    }
+    
+    /**
+     * Metóda odstraňuje hráčov z hry.
+     * 
+     * @return boolean vracia hodnotu true ak sa podarí odstrániť hráča
+     */
+    public boolean odstranHraca() {
+        if (this.pocetHracov <= 2) {
+            System.out.println("Musia byť minimálne dvaja hráči.");
+            return false;
+        }
+        
+        System.out.println("Znak hráča, ktorého chcete odstrániť: ");
+        char znak = this.input.next().charAt(0);
+        boolean jeOdstraneny = false;
+        
+        for (int i = 0; i < this.hraci.size(); i++) {
+            if (znak == this.hraci.get(i).getZnak()) {
+                this.hraci.remove(i);
+                this.pocetHracov--;
+                System.out.println("Hráč odstránený.");
+                jeOdstraneny = true;
+            }
+        }
+        
+        if (!jeOdstraneny) {
+            System.out.println("Hráča sa nepodarilo odstrániť. Skúste to znovu,");
+        }
+        return jeOdstraneny;
     }
     
     /**
